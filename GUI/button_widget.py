447e9dbfb4e74136bb7d29f3303b73cbe9c38067
Daniel-Bil <dxxx409@gmail.com>
2023-11-04 00:34:35 +0100

    update plan

--- GUI/button_widget.py
+++ GUI/button_widget.py
@@ -1,4 +1,8 @@
 
 
 
-pass
+"""
+
+Grid layout in widget with 4 x 4 grid of buttons
+
+"""
---
+++ GUI/parameters_widget.py
@@ -0,0 +1,6 @@
+"""
+
+Qline edits and labels informing how many decvices takees part in learning -> custom label + edit widgets
+
+
+"""
